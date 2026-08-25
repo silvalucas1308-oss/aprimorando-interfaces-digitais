06f0b5f6ea651ba3382e3f3fb8b7244ca57b2214
lucas <silva.lucas1308@escola.pr.gov.br>
2026-08-25 19:07:50 -0300

    copia.21

--- script.js
+++ script.js
@@ -18,11 +18,11 @@ curtiu = true;
 }
     } )
 
-    const btnTemaEscuro = document..querySelector(".btn-tema-escuro");
+    const btnTemaEscuro = document.querySelector(".btn-tema-escuro");
 
     btnTemaEscuro.addEventListener("click", mudaTema);
 
-function mudaTema() {
+    function mudaTema() {
     const corpoPagina = document.body;
     if (corpoPagina.classList.contains("tema-escuro")) {
         corpoPagina.classList.remove("tema-escuro");
